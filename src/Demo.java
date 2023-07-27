e49f47bf16777c07f64de1774c44f25afbd15902
imesh <bitwhizmail1@gmail.com>
2023-07-27 11:35:18 +0530

    All Implemented..

--- src/Demo.java
+++ src/Demo.java
@@ -2,10 +2,78 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
+
+import java.util.List;
 
 public class Demo {
     public static void main(String[] args) {
-        Customer c1 = new Customer(1,"Imesh","Galle",25000);
+        //saveCustomer();
+        //getAllCustomers();
+        //getCustomer(1);
+        //deleteCustomerById(1);
+        //deleteCustomerByObject(2);
+        updateCustomer();
+    }
+    private static void updateCustomer(){
+        Customer c1 = new Customer(2,"Isuranga","Galle",25000);
+
+        Configuration configuration = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Customer.class);
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session session = sessionFactory.openSession();
+
+        Transaction transaction = session.beginTransaction();
+
+        session.update(c1);
+        transaction.commit();
+        session.close();
+        sessionFactory.close();
+    }
+
+
+    private static void deleteCustomerById(long id){
+        Configuration configuration = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Customer.class);
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+
+        Query query = session.createQuery("DELETE Customer WHERE Customer_id=:id");
+        query.setParameter("id",id);
+        query.executeUpdate();
+        transaction.commit();
+    }
+
+    private static void deleteCustomerByObject(long id){
+        Configuration configuration = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Customer.class);
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+
+        session.delete(session.get(Customer.class,id));
+        transaction.commit();
+    }
+
+    private static void getCustomer(long id){
+        Configuration configuration = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Customer.class);
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session session = sessionFactory.openSession();
+
+        Customer customer = session.get(Customer.class, id);
+        System.out.println(customer);
+    }
+
+    private static void getAllCustomers(){
+        Configuration configuration = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Customer.class);
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session session = sessionFactory.openSession();
+
+        Query query = session.createQuery("FROM Customer");
+        List list = query.list();
+        System.out.println(list);
+    }
+
+    private static void saveCustomer(){
+        Customer c1 = new Customer(2,"Imesh","Galle",25000);
 
         Configuration configuration = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Customer.class);
         SessionFactory sessionFactory = configuration.buildSessionFactory();
@@ -17,6 +85,5 @@ public class Demo {
         transaction.commit();
         session.close();
         sessionFactory.close();
-
     }
 }
